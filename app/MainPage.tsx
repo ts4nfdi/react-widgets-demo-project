117a14372d73d1f3ab77cfa2902657e367726905
sasse <sasse@zbmed.de>
2024-04-02 16:16:42 +0200

    update main page

---
+++ app/MainPage.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { EuiFlexGroup, EuiFlexItem, EuiPage, EuiPageBody, EuiPanel, EuiSpacer, EuiTitle } from '@elastic/eui';
+import { AutocompleteWidget, BreadcrumbWidget, DataContentWidget, MetadataWidget } from '@nfdi4health/semlookp-widgets'
+
+export function MainPage() {
+    return (
+        <EuiPage paddingSize={"xl"}>
+            <EuiPageBody>
+                <EuiFlexGroup gutterSize="l" direction="column" justifyContent="spaceEvenly">
+                    <EuiFlexItem>
+                        <EuiFlexGroup gutterSize="l">
+                            <EuiFlexItem>
+                                <EuiPanel>
+                                    <EuiTitle><h3>Autocomplete Widget</h3></EuiTitle>
+                                    <EuiSpacer/>
+                                    <AutocompleteWidget
+                                        api="https://semanticlookup.zbmed.de/ols/api/"
+                                        hasShortSelectedLabel
+                                        parameter="collection=nfdi4health"
+                                        placeholder="Search for Term"
+                                        selectionChangedEvent={function noRefCheck() {
+                                        }}
+                                        allowCustomTerms={false}
+                                        singleSelection={false}/>
+
+                                </EuiPanel>
+                            </EuiFlexItem>
+                            <EuiFlexItem>
+                                <EuiPanel>
+                                    <EuiTitle><h3>DataContent Widget</h3></EuiTitle>
+                                    <EuiSpacer/>
+                                    <DataContentWidget api={"https://semanticlookup.zbmed.de/ols/api/"}/>
+                                </EuiPanel>
+                            </EuiFlexItem>
+                        </EuiFlexGroup>
+                    </EuiFlexItem>
+                    <EuiFlexItem>
+                        <EuiFlexGroup gutterSize="l">
+                            <EuiFlexItem>
+                                <EuiPanel>
+                                    <EuiTitle><h3>Metadata Widget</h3></EuiTitle>
+                                    <EuiSpacer/>
+                                    <MetadataWidget
+                                        api={"https://semanticlookup.zbmed.de/ols/api/"}
+                                        iri={"http://purl.obolibrary.org/obo/NCIT_C2985"}/>
+                                </EuiPanel>
+                            </EuiFlexItem>
+                            <EuiFlexItem>
+                                <EuiPanel>
+                                    <EuiTitle><h3>Breadcrumb Widget</h3></EuiTitle>
+                                    <EuiSpacer/>
+                                    <BreadcrumbWidget
+                                        api={"https://semanticlookup.zbmed.de/ols/api/"}
+                                        iri={"http://purl.obolibrary.org/obo/NCIT_C2985"}/>
+                                </EuiPanel>
+                            </EuiFlexItem>
+                        </EuiFlexGroup>
+                    </EuiFlexItem>
+                </EuiFlexGroup>
+            </EuiPageBody>
+        </EuiPage>
+    );
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,16 @@
 'use client';
 
 import { EuiProvider } from "@elastic/eui";
-import { AutocompleteWidget } from '@nfdi4health/semlookp-widgets'
 import { QueryClient, QueryClientProvider } from "react-query";
 import "@elastic/eui/dist/eui_theme_light.css";
+import { MainPage } from '@/app/MainPage'
 
 export default function Home() {
     const queryClient = new QueryClient();
     return (
         <EuiProvider>
             <QueryClientProvider client={queryClient}>
-                <AutocompleteWidget
-                    api="https://semanticlookup.zbmed.de/ols/api/"
-                    hasShortSelectedLabel
-                    parameter="collection=nfdi4health"
-                    placeholder="Search for Term"
-                    selectionChangedEvent={function noRefCheck() {
-                    }}
-                    allowCustomTerms={false}/>
+                <MainPage/>
             </QueryClientProvider>
         </EuiProvider>
     )
